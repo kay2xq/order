4151473353d5ed4265c3ef375c968ba45780e80d
kay <kay2xq@163.com>
2018-07-12 11:15:05 +0800

    新增缓存信息

--- service/src/main/java/com/imooc/message/ProductInfoReceive.java
+++ service/src/main/java/com/imooc/message/ProductInfoReceive.java
@@ -6,6 +6,8 @@ import com.imooc.utils.JsonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -17,11 +19,23 @@ import java.util.List;
 @Slf4j
 public class ProductInfoReceive {
 
+    private final static String PRODUCT_STOCK_TEMPLATE = "product_stock_%s";
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     @RabbitListener(queuesToDeclare = @Queue("product_stock"))
     public void process(String message){
         List<ProductInfoOutput> result = (List<ProductInfoOutput>)JsonUtils.JsonToObject(message,
                 new TypeReference<List<ProductInfoOutput>>() {});
+
         log.info("从队列中获取消息: {}", result);
+
+        //将库存信息存储到redis中
+        for (ProductInfoOutput p: result){
+            redisTemplate.opsForValue().set(String.format(PRODUCT_STOCK_TEMPLATE, p.getProductId()),
+                    String.valueOf(p.getProductStock()));
+        }
 
     }
 }
