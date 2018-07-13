02d3accaf55e2a5ed0b10599eafe3d139bf6d3b0
kay <kay2xq@163.com>
2018-07-13 22:54:26 +0800

    sleuth 结合 zipkin 实现方法监控的可视化

--- service/src/main/java/com/imooc/OrderApplication.java
+++ service/src/main/java/com/imooc/OrderApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableFeignClients(basePackages = "com.imooc.product")
 @SpringCloudApplication
 @ComponentScan(basePackages = "com.imooc")
+@EnableHystrixDashboard  // 对熔断的可视化主键
 public class OrderApplication {
 
 	public static void main(String[] args) {
